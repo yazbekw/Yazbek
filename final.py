3c00ed510d15a2667071957b772323821cce749f
yazbekw <yazbek965@gmail.com>
2025-10-08 09:21:09 +0300

    Update final.py

--- final.py
+++ final.py
@@ -385,9 +385,10 @@ class ScalpingTradingBot:
     _instance = None
     
     TRADING_SETTINGS = {
-        'symbols': ["ETHUSDT"],  # العملات المضافة
+        'symbols': ["ETHUSDT","SOLUSDT"],  # العملات المضافة
         'symbol_settings': {
         'ETHUSDT': {'used_balance': 8, 'leverage': 5, 'nominal_trade_size': 40},  # ✅
+        'SOLUSDT': {'used_balance': 8, 'leverage': 5, 'nominal_trade_size': 40},  # ✅   
         },
         'max_active_trades': 2,  # زيادة الحد إلى 4 صفقات
         'data_interval': '5m',   # 5 دقائق لاستراتيجية EMA+RSI
