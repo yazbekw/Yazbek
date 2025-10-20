3632c29efd947c74273f0f1c14766a110edc6acf
yazbekw <yazbek965@gmail.com>
2025-10-21 02:27:28 +0300

    Update ping1.py

--- ping1.py
+++ ping1.py
@@ -15,7 +15,7 @@ URLS = [
     "https://yazbek-3.onrender.com",
     "https://scanner-zwlt.onrender.com",
     "https://testnet-7t23.onrender.com",
-    "https://crypto-scalping.onrender.com"
+    "https://trade-hbwj.onrender.com"
 ]
 
 def send_pings():
